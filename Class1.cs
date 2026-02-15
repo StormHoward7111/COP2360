fd3eec2861d6a3cd93bcd121a3b0a77be72ae030
StormHoward7111 <stormhoward@outlook.com>
2026-02-15 15:33:37 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class OctopusCountdown : IEnumerator
+{
+	private int count;
+
+	public OctopusCountdown(Octopus o)
+	{
+		// Count down from # of legs on octopus
+		count = o.Legs + 1;
+	}
+
+	public bool MoveNext() => count-- > 0;
+
+	public object Current => count;
+
+	public void Reset()
+	{
+		throw new NotSupportedException();
+	}
+}
